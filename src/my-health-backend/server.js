5fbab5e5ed1e43320de8d0f75fa1a776c71eb858
Julia Mari Bernaus <jumaber@gmail.com>
2025-04-12 13:25:50 +0200

    changed mogoose connection

--- src/my-health-backend/server.js
+++ src/my-health-backend/server.js
@@ -11,10 +11,8 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
+
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
